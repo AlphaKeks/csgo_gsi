d49068abe4e320d0fc160f13812dd397d3b45295
AlphaKeks <alphakeks@dawn.sh>
2023-04-01 04:41:22 +0200

    Add README

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 //! Rust library for [CS:GO's GSI](https://developer.valvesoftware.com/wiki/Counter-Strike:_Global_Offensive_Game_State_Integration).
+//!
+//! I made this library because [csgo-gsi](https://crates.io/crates/csgo-gsi)'s `GSIServer` is not
+//! thread-safe (which makes 0 sense). This is simply a port of `csgo-gsi` with some small API
+//! changes + a [`Send`] + [`Sync`] [`GSIServer`] :)
 #![warn(rust_2018_idioms, missing_docs, missing_debug_implementations)]
 #![warn(clippy::style, clippy::complexity, clippy::cognitive_complexity)]
 #![deny(clippy::correctness, clippy::perf)]
@@ -9,6 +13,7 @@ pub use error::{Error, Result};
 
 /// Module containing all the datastructures for events received from CS:GO.
 pub mod event;
+pub use event::Event;
 
 /// Module for the GSI Configurations.
 mod config;
